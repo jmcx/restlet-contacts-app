c961fdca1558665a358e1ee3a7b1670cbcff78d3
jmcx <Jonathan@Jonathan-VAIO.(none)>
2013-11-13 14:06:12 +0100

    updated representations for snippets and links in contact list and details in contacts/uuid

--- src/contacts/utils/JsonUtils.java
+++ src/contacts/utils/JsonUtils.java
@@ -12,7 +12,7 @@ import contacts.resources.Contact;
 public class JsonUtils {
 
 	public static StringRepresentation representContactJson(Contact contact){
-		return new StringRepresentation(contactToJson(contact), MediaType.APPLICATION_JSON);
+		return new StringRepresentation(contactDetailToJson(contact), MediaType.APPLICATION_JSON);
 	}
 
 	public static StringRepresentation representContactsJson(HashMap<String,Contact> contacts){
@@ -20,7 +20,7 @@ public class JsonUtils {
 		Collection<Contact> values = contacts.values();
 		for (Iterator<Contact> iterator = values.iterator(); iterator.hasNext();) {
 			Contact c = (Contact) iterator.next();
-			s += contactToJson(c);
+			s += contactSnippetToJson(c);
 			if (iterator.hasNext()){
 				s += ",";
 			}	
@@ -30,7 +30,11 @@ public class JsonUtils {
 		return new StringRepresentation(s, MediaType.APPLICATION_JSON);
 	}
 
-	private static String contactToJson(Contact contact){
+	private static String contactSnippetToJson(Contact contact){
+		return "{ \"uuid\": \"" + contact.getUuid() + "\", \"name\": \"" + contact.getName() + "\", \"age\": " + contact.getAge() + ", \"details\": \"/" + contact.getUuid() + "\" }";
+	}
+	
+	private static String contactDetailToJson(Contact contact){
 		return "{ \"uuid\": \"" + contact.getUuid() + "\", \"name\": \"" + contact.getName() + "\", \"age\": " + contact.getAge() + ", \"email\": \"" + contact.getEmail() + "\" }";
 	}
 }
